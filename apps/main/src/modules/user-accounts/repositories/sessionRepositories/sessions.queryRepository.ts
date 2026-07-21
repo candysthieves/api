2be3b858c011dc8628c9b4d78d67913776331a6f
ChiteS <balbesik-07@mail.ru>
2026-07-21 22:29:15 +0300

    Add Session schema, "entity" and getAllSessionsForUser endpoint. Fix dependencies.

---
+++ apps/main/src/modules/user-accounts/repositories/sessionRepositories/sessions.queryRepository.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../../infrastructure/prisma/prisma.service.js';
+import type { Session } from '../../../../generated/prisma/client.js';
+
+@Injectable()
+export class SessionsQueryRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findSessionsForUser(userId: string): Promise<Session[]> {
+    return this.prisma.session.findMany({
+      where: { userId },
+    });
+  }
+}
