1617cbab7f9d64c6b55ae20bf0ae7ba5f48932ea
ChiteS <balbesik-07@mail.ru>
2026-07-31 19:20:44 +0300

    FIX #3 fix tests

--- apps/main/test/app.e2e-spec.ts
+++ apps/main/test/app.e2e-spec.ts
@@ -2,15 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types.js';
-import { AppModule } from '../src/app.module.js';
+import { PrismaService } from '../src/infrastructure/prisma/prisma.service.js';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
+    process.env.DATABASE_URL = 'postgresql://test:test@localhost:5432/test';
+    const { AppModule } = await import('../src/app.module.js');
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue({})
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
@@ -20,6 +25,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Main!');
   });
 });
