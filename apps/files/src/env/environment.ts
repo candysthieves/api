3d8cbc21473fb1b05d39bcb0881d17bd5dfad210
VladSapozhnik <vladbars2@gmail.com>
2026-08-31 19:41:18 +0300

    fix

--- apps/files/src/env/environment.ts
+++ apps/files/src/env/environment.ts
@@ -1,5 +1,14 @@
-import { Type } from 'class-transformer';
-import { IsInt, IsNotEmpty, IsString, Max, Min } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Max,
+  Min,
+} from 'class-validator';
 
 export class EnvironmentVariables {
   @Type(() => Number)
