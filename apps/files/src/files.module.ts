6e25d749c1fdc065dab786ceefcbfd637e67d90d
VladSapozhnik <vladbars2@gmail.com>
2026-08-31 18:59:25 +0300

    fix url

--- apps/files/src/files.module.ts
+++ apps/files/src/files.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { configModule } from './config.js';
-import { FilesController } from './files.controller.js';
 import { FilesConfig } from './files.config.js';
 import { RabbitMqModule } from './rabbitmq/rabbitmq.module.js';
-import { FilesService } from './files.service';
+import { FilesService } from './modules/files/application/files.service.js';
+import { FilesController } from './modules/files/api/files.controller.js';
 
 @Module({
   imports: [configModule, RabbitMqModule],
