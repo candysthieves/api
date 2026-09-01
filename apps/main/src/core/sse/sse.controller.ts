3879574e23095e04c37584efab44f9e4bae99eb7
VladSapozhnik <vladbars2@gmail.com>
2026-09-01 18:30:07 +0300

    add docs for see point

--- apps/main/src/core/sse/sse.controller.ts
+++ apps/main/src/core/sse/sse.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, MessageEvent, Sse } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
 import { SseService } from './sse.service.js';
+import { ApiSseEvents } from '../swagger/sse-dto/sse-events.swagger.js';
 
 @Controller('events')
 export class SeeController {
   constructor(private readonly sseService: SseService) {}
 
   @Sse()
+  @ApiSseEvents()
   events(): Observable<MessageEvent> {
     return this.sseService.getEvents().pipe(
       map((event) => ({
